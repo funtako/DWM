810a815be36b80f93eab548e155b69adcfd9e7b6
funtako <88504814+funtako@users.noreply.github.com>
2022-02-02 01:31:30 +0000

    Add files via upload

--- config.h
+++ config.h
@@ -40,6 +40,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
     { "ncmpcpp",  NULL,       NULL,       1 << 7,                 0,           -1 },
+    { "Brave-browser",  NULL,       NULL,       1 << 1,                 0,           -1 },
 };
 
 /* layout(s) */
